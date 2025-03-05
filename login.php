17cabade7c51312c178a6f99c59459a5c1e5a2f2
graceparker <Grace.Parker@verisglobal.com>
2025-03-05 16:35:42 -0600

    test

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 // Assuming you have predefined username and password
-$valid_username = "your_username";
-$valid_password = "your_password";
+$valid_username = "jcastillo@primermg.com";
+$valid_password = "Prime#2025";
 
 // Retrieve username and password from the form
 $username = $_POST['username'];
@@ -9,11 +9,13 @@ $password = $_POST['password'];
 
 // Check if username and password match
 if ($username === $valid_username && $password === $valid_password) {
+    $_SESSION['isJuan'] = $true;
     // Authentication successful, redirect to the contact form
-    header("Location: contact_form.php");
+    header("Location: index.html");
     exit();
 } else {
     // Authentication failed, redirect back to the login page
+    $_SESSION['isJuan'] = false;
     header("Location: login.html");
     exit();
 }
